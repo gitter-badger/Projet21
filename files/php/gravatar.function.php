2446475cf95fe0e1f2c5450862402669505f9b44
Ternoc <euvfteam.hhha2000@gmail.com>
2015-05-19 12:59:28 +0200

    gh pages : initial commit

--- files/php/gravatar.function.php
+++
@@ -1,8 +0,0 @@
-<?php
-function gravatarUrl($email, $size = "80", $rating = "g")
-{
-    $hash = md5(strtolower(trim($email)));
-    $url = 'https://gravatar.com/avatar/' . $hash . '?s=' . $size . '&amp;r=' . $rating . '&amp;d=identicon';
-    return $url;
-}
-?>
